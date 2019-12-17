1b175138d3b4a5f5f22033e1094b79cb7efc3c8b
naomiskevo <naomiskevo@gmsail.com>
2019-12-17 16:59:51 -0600

    -Sets up database boilerplate and models with fake data 	-new file:   db.sqlite3 	-new file:   main_app/__init__.py 	-new file:   main_app/admin.py 	-new file:   main_app/apps.py 	-new file:   main_app/migrations/__init__.py 	-new file:   main_app/models.py 	-new file:   main_app/tests.py 	-new file:   main_app/urls.py 	-new file:   manage.py 	-new file:   wigcollector/__init__.py 	-new file:   wigcollector/wsgi.py 	-new file:   wigcollector/settings.py 	-new file:   wigcollector/urls.py
    -Adds styling to all pages except home page
    	-new file:   main_app/static/css/style.css
    
    -Establishes partial views for app
    	-new file:   main_app/templates/base.html
    
    -Adds template for about page
    	-new file:   main_app/templates/about.html
    
    -Adds template for wigs/index page and shows list of all wigs
    	-new file:   main_app/templates/wigs/index.html

---
+++ main_app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+class Wig:
+  def __init__(self, name, origin, description, length):
+    self.name = name
+    self.origin = origin
+    self.description = description
+    self.length = length
+
+wigs = [
+    Wig('Victoria', 'Brazilian', 'dark brown body wave', 22),
+    Wig('Candace', 'Synthetic', 'rainbow blunt cut bob', 12),
+    Wig('Gladys', 'Persian', 'black curly hair', 18),
+]
+# Create your views here.
+def home(request):
+    return HttpResponse('<h1>Wig Collector</h1>')
+
+def about(request):
+    return render(request, 'about.html')
+
+def wigs_index(request):
+    return render(request, 'wigs/index.html', { 'wigs': wigs})
