2b050b594e7acb91e1c6287907cfd08e56b38d76
naomiskevo <naomiskevo@gmsail.com>
2019-12-17 20:47:57 -0600

    adds url path for individual cat page to url_patterns list

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('wigs/', views.wigs_index, name='index'),
+    path('wigs/<int:wig_id>/', views.wigs_detail, name='detail'),
 ]
