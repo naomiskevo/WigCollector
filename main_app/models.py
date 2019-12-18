1c7054479eb5b43e8d3b9493b4fbe4e70368277f
naomiskevo <naomiskevo@gmsail.com>
2019-12-17 20:31:46 -0600

    adds Wig model and __str__ function

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Wig(models.Model):
+    name = models.CharField(max_length=100)
+    origin = models.CharField(max_length=100)
+    description = models.TextField(max_length=200)
+    length = models.IntegerField()
+
+    def __str__(self):
+        return self.name
