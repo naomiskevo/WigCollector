457fdcdb53901253d62826bc9d63defd9eb7ebd8
naomiskevo <naomiskevo@gmsail.com>
2019-12-17 20:17:20 -0600

    changes home function to render home template

--- main_app/views.py
+++ main_app/views.py
@@ -15,7 +15,7 @@ wigs = [
 ]
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Wig Collector</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
