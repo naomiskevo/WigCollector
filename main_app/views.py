6aa2182a884f3c8a6f01e7ac65232044b3578753
naomiskevo <naomiskevo@gmsail.com>
2019-12-17 20:34:34 -0600

    changes views page to import Wig model from the database

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Wig
 
-class Wig:
-  def __init__(self, name, origin, description, length):
-    self.name = name
-    self.origin = origin
-    self.description = description
-    self.length = length
 
-wigs = [
-    Wig('Victoria', 'Brazilian', 'dark brown body wave', 22),
-    Wig('Candace', 'Synthetic', 'rainbow blunt cut bob', 12),
-    Wig('Gladys', 'Persian', 'black curly hair', 18),
-]
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -21,4 +10,5 @@ def about(request):
     return render(request, 'about.html')
 
 def wigs_index(request):
+    wigs = Wig.objects.all()
     return render(request, 'wigs/index.html', { 'wigs': wigs})
