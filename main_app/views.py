e1d961587121ddca4718a21921466fc5c7373015
naomiskevo <naomiskevo@gmsail.com>
2020-01-06 11:04:04 -0600

    adds signup view finction

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Wig, Type, Photo
 from .forms import ConditionForm
 import uuid
@@ -92,3 +94,17 @@ def add_photo(request, wig_id):
         except:
             print('An error occurred uploading file to S3')
     return redirect('detail', wig_id=wig_id)
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+    if form.is_valid():
+        user = form.save()
+        login(request, user)
+        return redirect('index')
+    else:
+        error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
